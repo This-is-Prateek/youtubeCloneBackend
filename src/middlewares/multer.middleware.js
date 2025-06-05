42c38471b50eb9c6bb881723a3212491a8b51038
Prateek Kumawat <kumawatprateek01@gmail.com>
2025-06-05 21:06:24 +0530

    middleware update

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,17 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({         //saves uploaded file in local disk
-    destination: function (req, file, cb) {  //destination path where the file should be saved
-        cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {       //uploaded filename
-        cb(null, Date.now() + file.originalname);
-    }
-})
+let storage
+try {
+    storage = multer.diskStorage({         //saves uploaded file in local disk
+        destination: function (req, file, cb) {  //destination path where the file should be saved
+            cb(null, './public/temp')
+        },
+        filename: function (req, file, cb) {       //uploaded filename
+            cb(null, Date.now() + file.originalname);
+        }
+    })
+} catch (error) {
+    console.log("Error in multer middleware: ", error.message);
+}
 
 export const upload = multer({ storage })   //initializes multer middleware with specified config
